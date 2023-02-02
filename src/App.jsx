56d60fdd8afc0c11159c2d2cca240854c24f5e40
Fosslover69 <no.1opensourcelover@protonmail.com>
2023-02-02 19:14:07 +0530

    chatgpt: Switch to async await

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App() {
   };
 
   const sendOpenAI = async (input) => {
-    fetch('/openai', {
+    await fetch('/openai', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         },
         body: JSON.stringify({input: input})
         })
-        .then(response => response.json())
+        .then(response =>  response.json())
         .then(data => {
           setResult(data.message);
         }
